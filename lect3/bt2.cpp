619cd09d9e78673a1c318a048f27f8f0d7d754a4
23021416 <dinhngoclong7a@gmail.com>
2025-03-08 11:56:27 +0700

    Update bt2.cpp

--- lect3/bt2.cpp
+++ lect3/bt2.cpp
@@ -1,30 +1,5 @@
-#include <iostream>
 
-int hammu2(int n){
-    // sử dụng đệ quy
-    if(n==0) return 1;
-    return 2*hammu2(n-1);
-}
-
-int hamnhanh2(int x, int n){
-    if(n==0) return 1;
-    if(n%2==0) {
-        int y = hamnhanh2(x, n/2);
-        return y*y;
-    } else{
-        int y = hamnhanh2(x, (n-1)/2);
-        return y*y*x;
-    }
-
-}
-
-int main() {
-    int i; std::cin >> i;
-    std::cout << hammu2(i);
-    return 0;
-}
-
-/* Mã giả
+Mã giả
 Hàm hamnu2(n: số nguyên dương)
     Nếu n = 0
         trả về 1
@@ -32,4 +7,13 @@ Hàm hamnu2(n: số nguyên dương)
         trả về 2*hamnu2(n-1)
     Độ phức tạp: O(n)
 
-*/ 
+
+HÀM hamnhanh2(x, n):
+    Nếu n == 0:
+        Trả về 1
+    Nếu n chẵn:
+        y = hamnhanh2(x, n/2)
+        trả về y * y
+    Nếu n lẻ:
+        y = hamnhanh2(x, (n-1)/2)
+        trả về y * y * x
